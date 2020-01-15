2a6ab8a07b51c4480d5070e29759c4d92366947f
Marc Stober <marcstober@gmail.com>
2020-01-15 07:41:34 -0500

    add README, gitignore additions

---
+++ README.md
@@ -0,0 +1,20 @@
+
+# SETUP:
+1. pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+  (I used virtualenv instead of installing as admin. (.\venv/Scripts\activate))
+2. Although this is accessing public data we're using an API that requires authentication. 
+  Go to https://developers.google.com/sheets/api/quickstart/python, create an application, then
+  download a credentials.json file. Then the first time you run this you'll be prompted to log in
+  in your browser.
+  (TODO: find a way to not require authentication, maybe publishing with the old API 
+  as described here: https://stackoverflow.com/a/45731424 
+  or using the chart API described here: https://stackoverflow.com/a/33727897)
+
+# USAGE:
+  (first time only:) mkdir ../miqra-data/
+  python downloadcsv.py
+CSV files for each sheet will be saved in miqra-data folder.
+
+# Troubleshooting:
+
+If you get an error try deleting token.pickle (and you will have to log in to Google Sheets again)
--- downloadcsv.py
+++ downloadcsv.py
@@ -8,22 +8,6 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
-# SETUP:
-# 1. pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
-#   (I used virtualenv instead of installing as admin.)
-# 2. Although this is accessing public data we're using an API that requires authentication. 
-#   Go to https://developers.google.com/sheets/api/quickstart/python, create an application, then
-#   download a credentials.json file. Then the first time you run this you'll be prompted to log in
-#   in your browser.
-#   (TODO: find a way to not require authentication, maybe publishing with the old API 
-#   as described here: https://stackoverflow.com/a/45731424 
-#   or using the chart API described here: https://stackoverflow.com/a/33727897)
-
-# USAGE:
-#   (first time only:) mkdir ../miqra-data/
-#   python downloadcsv.py
-# CSV files for each sheet will be saved in miqra-data folder.
-
 # See also: https://stackoverflow.com/a/51235960
 
 class Downloader:
