1d86139e826913e1000ab9b05f28c01cc1669085
Marc Stober <marcstober@gmail.com>
2020-06-17 13:57:35 -0400

    add some comments to latest code

--- fixtsvquotesforgithub.py
+++ fixtsvquotesforgithub.py
@@ -1,6 +1,11 @@
 import csv
 import sys
 
+# To show TSV files nicely, Github requires any cell that has a quotation mark in it to:
+#   1. have the whole cell surrounded with quotation marks
+#   2. have any question marks doubled
+# The "excel-tab" dialect built into the Python csv module does this.
+
 filepath = sys.argv[1]
 
 print(filepath)
